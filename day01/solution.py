5ef528a4864817eda0847e39ace19c185c2ecdc0
CHELSEA SHELIN PURNARIA <chelsiecute.cc@gmail.com>
2025-09-05 08:34:25 +0800

    update

--- day01/solution.py
+++ day01/solution.py
@@ -1,8 +1,9 @@
-# def reverse_string(s: str) -> str:
-#     """Mengembalikan string s dalam urutan terbalik."""
-#     return s[::-1]
+def reverse_string(s: str) -> str:
+    """Mengembalikan string s dalam urutan terbalik."""
+    return s[::-1]
 
-# if __name__ == "__main__":
-#     # Contoh penggunaan
-#     test = "hello"
-#     print(reverse_string(test))  # Output: olleh
+if __name__ == "__main__":
+    # Contoh penggunaan
+    test = "hello"
+    print(reverse_string(test))  # Output: olleh
+  
